6a58e7652eabbe8aea48a40bc786aeb78c9eb383
Victor Pinas Arnault <victor.arnault@yahoo.com>
2022-04-03 20:12:53 -0300

    Configurando ProjetoPanis

--- ProjetoPanis.java
+++ ProjetoPanis.java
@@ -2,7 +2,7 @@ public class ProjetoPanis{
   public static void main(String[] args){
   
   //Declara e cria um objeto cliente e pedido
-  Cliente pedidoCliente = new Cliente();
+  Cliente pedidoCliente = new Cliente(0,0);
   Pedido = new Pedido(pedidoCliente);
   
   //Mostre ao cliente e quantos pães ele pediu
@@ -10,9 +10,16 @@ public class ProjetoPanis{
   System.out.println("Pedido recebido: " + getEnvio());
   
   //Mostre em quanto tempo os pães ficarão prontos
-  System.out.println("Esse é o tempo que levará para os pães ficarem prontos, por favor aguarde." + pedido.tempo);
+  System.out.println("Esse é o tempo que levará para os pães ficarem prontos, por favor aguarde." + entrega.tp);
+  System.out.println("Essa foi a quantia de pães que você pediu: " + entrega.qtia);
   
-  // Estabeleça o tempo
-  pedido.tempo = tempoEspera;
+  //configurar pedido
+  entrega.pedido = clientePedido;
+  
+  //realizar entrega
+  clientePedido.entrega(0,0);
+  System.out.println("Esse é o tempo que levará para os pães ficarem prontos, por favor aguarde." + entrega.tp);
+  System.out.println("Essa foi a quantia de pães que você pediu: " + entrega.qtia);
+  
   }
 }
