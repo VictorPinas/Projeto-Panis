66863aa83f5c42583772b6a9138090a2838d8d5b
Victor Pinas Arnault <victor.arnault@yahoo.com>
2022-04-24 18:48:15 -0300

    Programa tempo.java completo

--- Tempo.java
+++ Tempo.java
@@ -1,3 +1,18 @@
 public class Tempo{
-
+  public static void main(String[] args){
+    String[][] tempo = {
+      {"4", "2",  " min"},
+      {"5", "8", " seg"}
+    };
+    // 42 min
+    System.out.println(tempo[0][0] + tempo[0][1] + tempo[0][2]);
+    
+    // 58 seg
+    System.out.println(tempo[1][0] + tempo[1][1] + tempo[1][2]);
+  }
+  // construtor
+  public Tempo(String m,String s){
+    m = " min";
+    s = " seg";
+  }
 }
